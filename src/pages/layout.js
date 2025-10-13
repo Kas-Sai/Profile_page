bb705d6f39365a75c82a68dc7d9f3f71ac1d6e4f
Sahil Kashyap <115712131+Kas-Sai@users.noreply.github.com>
2025-10-13 23:44:11 +0530

    Update layout.js

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,5 +1,5 @@
 // app/layout.js
-import "@/styles/globals.css";
+// import "@/styles/globals.css";
 import ScrollToTop from "@/Components/ScrollToTop";
 
 export default function RootLayout({ children }) {
